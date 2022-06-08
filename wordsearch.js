4e10004be19189e5b339f5d6a26681f255097698
Ricardo Moura <ricardomoura1979@gmail.com>
2022-06-08 18:38:27 +0000

    updated

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,43 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const horizontalWordSearch = (letters, word) => {
+  if (!letters) {
+    return false
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+      //console.log("l = " + l); console.log("word = " + word);
+      if (l.includes(word)) {
+          return true;
+      }
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose2DArray = (letters) => {
+  let result = [];
+
+  if (!letters[0]) {
+    return false
+  }
+
+  for (let i = 0; i < letters[0].length; i++) {
+      result.push([]);
+      for (let j = 0; j < letters.length;
+    j++) {
+          result[i][j] = letters[j][i];
+      }
+  }
+  console.log (result);
+  return result;
+};
+
+const wordSearch = (letters, word) => {
+  let result = horizontalWordSearch(letters, word);
+  if (!result) {
+    let allTransposeLetters = transpose2DArray(letters);
+    result = horizontalWordSearch(allTransposeLetters, word);
+  }
+  return result;
+
+};
+
+module.exports = wordSearch;
